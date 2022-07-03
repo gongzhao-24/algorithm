08c0d7f09d2ddbcd4392cd3a47004bb0aec21272
gongzhao <543877051@qq.com>
2022-07-03 10:07:03 +0800

    fix

---
+++ 动态规划/NthSuperUglyNumber.java
@@ -0,0 +1,20 @@
+/**
+* 描述：
+* 创建日期：2022年07月01 18:43:
+* @author gong zhao 
+**/
+package 动态规划;
+
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+
+public class NthSuperUglyNumber {
+    public int nthSuperUglyNumber(int n, int[] primes) {
+        Set<Integer> set = new HashSet<>();
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>();
+        while (true) {
+            
+        }
+    }
+}
