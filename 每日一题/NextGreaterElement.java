b7a8db82f975902ccbac6508a4d667909046328c
gongzhao <543877051@qq.com>
2022-07-04 17:16:37 +0800

    fix

---
+++ 每日一题/NextGreaterElement.java
@@ -0,0 +1,13 @@
+/**
+* 描述：leetcode 556. 下一个更大元素 III
+* 创建日期：2022年07月03 19:59:
+* @author gong zhao 
+**/
+package 每日一题;
+
+public class NextGreaterElement {
+    public int nextGreaterElement(int n) {
+        // 下一个更大的元素，
+        return 0;
+    }
+}
